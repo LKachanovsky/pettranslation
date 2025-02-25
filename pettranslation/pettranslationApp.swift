64527136919547b346f835f89e4ab89598374783
Leo Kachanovskyi <leonid.kachanovsky@gmail.com>
2025-02-25 10:16:27 +0200

    Initial Commit

---
+++ pettranslation/pettranslationApp.swift
@@ -0,0 +1,17 @@
+//
+//  pettranslationApp.swift
+//  pettranslation
+//
+//  Created by Leo Kachanovskyi on 25.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct pettranslationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
